ed08d2606865742a40b7d4b4d2fdb6caa1d28d43
foobar <foobar@example.com>
2024-01-19 19:24:49 +0300

    N1

---
+++ load.py
@@ -0,0 +1,7 @@
+import pygame as pg
+from script import load_image
+topor_image = pg.image.load('image/topor/1.png')
+player_image1 = load_image('image/player/bottom')
+player_image2 = load_image('image/player/top')
+player_image3 = load_image('image/player/left')
+player_image4 = load_image('image/player/right')
