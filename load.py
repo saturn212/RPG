0d2c4d4981d22f640d31cf821e793aad4101483b
foobar <foobar@example.com>
2024-01-26 19:26:36 +0300

    N3

--- load.py
+++ load.py
@@ -1,7 +1,11 @@
 import pygame as pg
 from script import load_image
-topor_image = pg.image.load('image/topor/1.png')
+topor_image = pg.image.load('image/topor/1.png').convert_alpha()
 player_image1 = load_image('image/player/bottom')
 player_image2 = load_image('image/player/top')
 player_image3 = load_image('image/player/left')
 player_image4 = load_image('image/player/right')
+block_image = pg.image.load('image/block/block.jpg').convert_alpha()
+water_image = pg.image.load('image/block/water.jpg').convert_alpha()
+spawner_image = pg.image.load('image/block/spawner.png').convert_alpha()
+spider_image = load_image('image/spider')
--- main.py
+++ main.py
@@ -43,6 +43,7 @@ class Topor(pg.sprite.Sprite):
         self.moove()
 
 
+
 class Player(pg.sprite.Sprite):
     def __init__(self, image, pos,):
         pg.sprite.Sprite.__init__(self)
@@ -60,7 +61,7 @@ class Player(pg.sprite.Sprite):
         self.score = 0
         self.topor = 2
         self.camera = False
-
+        self.dir = 'bottom'
         self.add_topor()
     def add_topor(self):
         global topor_group
@@ -81,105 +82,215 @@ class Player(pg.sprite.Sprite):
 
     def update(self):
         self.animation()
+
         key = pg.key.get_pressed()
         if key[pg.K_w]:
             self.image = player_image2[self.frame]
             self.rect.y -= self.speed
             self.dir = "top"
             self.anime = True
+            if self.rect.top < 300:
+                self.pos_maps[0] += self.speed
+                camera_group.update(0, self.speed)
+                self.rect.top = 300
+                self.camera = True
+            else:
+                self.camera = False
         elif key[pg.K_a]:
             self.image = player_image3[self.frame]
             self.rect.x -= self.speed
             self.dir = "left"
             self.anime = True
+            if self.rect.left < 300 and self.pos_maps[0] < 0:
+                self.pos_maps[0] += self.speed
+                camera_group.update(self.speed, 0)
+                self.rect.left = 300
+                self.camera = True
+            else:
+                self.camera = False
         elif key[pg.K_s]:
             self.image = player_image1[self.frame]
             self.rect.y += self.speed
             self.dir = "bottom"
             self.anime = True
+            if self.rect.bottom > 600:
+                self.pos_maps[0] += self.speed
+                camera_group.update(0, -self.speed)
+                self.rect.bottom = 600
+                self.camera = True
+            else:
+                self.camera = False
         elif key[pg.K_d]:
             self.image = player_image4[self.frame]
             self.rect.x += self.speed
             self.dir = "right"
             self.anime = True
+            if self.rect.right > 900 and self.pos_maps[0] > -6800:
+                self.pos_maps[0] += self.speed
+                camera_group.update(-self.speed, 0)
+                self.rect.right = 900
+                self.camera = True
+            else:
+                self.camera = False
         else:
+            self.camera = False
             self.anime = False
+            self.image = player_image1[0]
 
 
 
 
 
 
+class Block(pg.sprite.Sprite):
+
+    def __init__(self, image, pos):
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+    def update(self, stepx, stepy):
+        self.rect.x += stepx
+        self.rect.y += stepy
+
+        if pg.sprite.spritecollide(self, player_group, False):
+            if player.dir == "left":
+                player.rect.left = self.rect.right
+            elif player.dir == "top":
+                player.rect.top = self.rect.bottom
+
+            elif player.dir == "bottom":
+                player.rect.bottom = self.rect.top
+            elif player.dir == "right":
+                player.rect.right = self.rect.left
 
 
+class Water(pg.sprite.Sprite):
 
+    def __init__(self, image, pos):
+
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self, stepx, stepy):
+        self.rect.x += stepx
+        self.rect.y += stepy
+        if pg.sprite.spritecollide(self, player_group, False):
+            if player.dir == "left":
+                player.rect.left = self.rect.right
+            elif player.dir == "top":
+                player.rect.top = self.rect.bottom
+            elif player.dir == "bottom":
+                player.rect.bottom = self.rect.top
+            elif player.dir == "right":
+                player.rect.right = self.rect.left
+
+class Spawner (pg.sprite.Sprite):
+
+    def __init__(self, image, pos):
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def update(self, stepx, stepy):
+        self.rect.x += stepx
+        self.rect.y += stepy
+        if pg.sprite.spritecollide(self, player_group, False):
+            if player.dir == "left":
+                player.rect.left = self.rect.right
+            if player.dir == "top":
+                player.rect.top = self.rect.bottom
+            elif player.dir == "bottom":
+                player.rect.bottom = self.rect.top
+            elif player.dir == "right":
+                player.rect.right = self.rect.left
+
+class Spider (pg.sprite.Sprite):
+
+    def __init__(self, image, pos):
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 
 
 def lvlGAME():
     screen.fill(BLaCK)
-
-
-
     player_group.update()
     player_group.draw(screen)
+    block_group.draw(screen)
     topor_group.update()
+    spawner_group.draw(screen)
+    spider_group.update()
+    spider_group.draw(screen)
+    water_group.draw(screen)
     topor_group.draw(screen)
-    # def startMenu():
-    #     screen.fill((122, 122, 122))
-    #
-    #     button_group.draw(screen)
-    #     button_group.update()
-    #
-    #     pg.display.update()
+    if not player.camera:
 
-    # def DrawMaps(nameFile):
-    #     maps = []
-    #     source = "game lvl/" + str(nameFile)
-    #     with open(source, "r") as file:
-    #         for i in range(0, 20):
-    #             maps.append(file.readline().replace("\n", "").split(",")[0:-1])
-    #     print(maps)
-    #     pos = [0, 0]
-    #     for i in range(0, len(maps)):
-    #         pos[1] = i * 40
-    #         for j in range(0, len(maps[0])):
-    #             pos[0] = 40 * j
-    #             if maps[i][j] == '3':
-    #                 brick = Brick(brick_image, pos)
-    #                 brick_group.add(brick)
-    #             elif maps[i][j] == '4':
-    #                 bush = Bush(bush_image, pos)
-    #                 bush_group.add(bush)
-    #             elif maps[i][j] == '2':
-    #                 iron = Iron(iron_image, pos)
-    #                 iron_group.add(iron)
-    #             elif maps[i][j] == '1':
-    #                 water = Water(water_image, pos)
-    #                 water_group.add(water)
-    #             elif maps[i][j] == '6':
-    #                 enemy = Enemy(enemy_image, pos)
-    #                 enemy_group.add(enemy)
-    #             elif maps[i][j] == '5':
-    #                 flag = Flag(flag_image, pos)
-    #                 flag_group.add(flag)
-    #
+        block_group.update(0, 0)
+
+        water_group.update(0, 0)
+
+        spawner_group.update(0, 0)
     pg.display.update()
 
+def DrawMaps(nameFile):
+    maps = []
+    source = "gamelvl/" + str(nameFile)
+    with open(source, "r") as file:
+        for i in range(0, 100):
+            maps.append(file.readline().replace("\n", "").split(",")[0:-1])
+    print(maps)
+    pos = [0, 0]
+    for i in range(0, len(maps)):
+        pos[1] = i * 40
+        for j in range(0, len(maps[0])):
+            pos[0] = 40 * j
+            if maps[i][j] == '2':
+                block = Block(block_image, pos)
+                block_group.add(block)
+                camera_group.add(block)
+            elif maps[i][j] == '1':
+                water = Water(water_image, pos)
+                water_group.add(water)
+                camera_group.add(water)
+            elif maps[i][j] == '3':
+                spawner = Spawner(spawner_image, pos)
+                spawner_group.add(spawner)
+                camera_group.add(spawner)
+            elif maps[i][j] == '6':
+                spider = Spider(spider_image, pos)
+                spider_group.add(spider)
+
+
 def restart():
-    global player_group,topor_group, player
+    global player_group,topor_group, block_group, water_group, spawner_group, spider_group, camera_group,player
     topor_group = pg.sprite.Group()
     player_group = pg.sprite.Group()
+    block_group = pg.sprite.Group()
+    water_group = pg.sprite.Group()
+    spawner_group = pg.sprite.Group()
+    spider_group = pg.sprite.Group()
+    camera_group = pg.sprite.Group()
+
     player = Player(player_image1[0], (300, 300))
     player_group.add(player)
 
-
-
-
-
-
-
 restart()
+
+
+
+
+
+DrawMaps('1.txt')
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
